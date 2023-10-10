6bb5bd1c693e81022e3c032380e475fd99f3f8f5
Will Kim <will@chequer.io>
2023-10-11 00:11:59 +0900

    feat: java enum copy 해서 enum 생성하는 스크립트 추가

---
+++ src/api/enums.js
@@ -0,0 +1,25 @@
+export const Enums = {
+  AuthVendor: {
+    GOOGLE: 'GOOGLE',
+    KAKAO: 'KAKAO',
+    NAVER: 'NAVER',
+  },
+
+  ProductType: {
+    FOOD: 'FOOD',
+    ACCOMMODATION: 'ACCOMMODATION',
+    TICKET: 'TICKET',
+  },
+
+  ReservationStatus: {
+    PROGRESS: 'PROGRESS',
+    CANCEL: 'CANCEL',
+    COMPLETION: 'COMPLETION',
+  },
+
+  UserRole: {
+    GENERAL: 'GENERAL',
+    SELLER: 'SELLER',
+    ADMIN: 'ADMIN',
+  },
+};
