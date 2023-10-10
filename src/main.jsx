81d1c5a9efb894cbd22613f20216bc55d5556c9a
Will Kim <will@chequer.io>
2023-10-11 02:11:56 +0900

    feat: ErrorBoundary 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import ErrorBoundary from './components/ErrorBoundary';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import './setup';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>,
+);
