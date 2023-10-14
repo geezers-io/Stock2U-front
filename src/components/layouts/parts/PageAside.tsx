baf993908c5490106bd238de6049aba5ab4df2e7
Will Kim <will@chequer.io>
2023-10-15 00:38:53 +0900

    fix: 잘못된 tsconfig 수정, emotion theme d.ts 추가 (#9)

--- src/components/layouts/parts/PageAside.tsx
+++ src/components/layouts/parts/PageAside.tsx
@@ -1,25 +1,27 @@
 import { FC, PropsWithChildren } from 'react';
 import { Box } from '@chakra-ui/react';
-import { APP_STYLES } from '@/styles/theme';
+import { useTheme } from '@emotion/react';
 
 const PageAside: FC<PropsWithChildren> = ({ children }) => {
+  const { appStyles } = useTheme();
+
   return (
     <Box
       as="aside"
-      width="100%"
-      height={APP_STYLES.ASIDE_HEIGHT}
+      w="100%"
+      h={appStyles.asideHeight}
       position="fixed"
       bottom={0}
       boxShadow="0 -2px 2px -2px rgba(0,0,0,.3)"
       backgroundColor="white"
     >
       <Box
-        height="100%"
-        maxWidth={APP_STYLES.MAX_WIDTH}
+        h="100%"
+        maxW={appStyles.maxWidth}
         display="flex"
         alignItems="center"
         margin="0 auto"
-        padding={`0 ${APP_STYLES.PADDING_X}`}
+        padding={`0 ${appStyles.paddingX}`}
       >
         {children}
       </Box>
