547b6e6da795e51d48f60ba87aef4117bfc6b129
Will Kim <will@chequer.io>
2023-10-15 02:38:22 +0900

    feat: code-verification 페이지 라우트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import AuthLayout from '@/components/layouts/Auth';
 import ServiceLayout from '@/components/layouts/Service';
+import CodeVerificationPage from '@/pages/auth/code-verification/page';
 import GeneralJoinPage from '@/pages/auth/join/general/page';
 import JoinPage from '@/pages/auth/join/page';
 import SellerJoinPage from '@/pages/auth/join/seller/page';
@@ -44,6 +45,10 @@ const router = createBrowserRouter([
             element: <SellerJoinPage />,
           },
         ],
+      },
+      {
+        path: 'code-verification',
+        element: <CodeVerificationPage />,
       },
     ],
   },
---
+++ src/pages/auth/code-verification/page.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import { Box } from '@chakra-ui/react';
+
+const CodeVerificationPage: FC = () => {
+  return <Box>CodeVerificationPage</Box>;
+};
+
+export default CodeVerificationPage;
