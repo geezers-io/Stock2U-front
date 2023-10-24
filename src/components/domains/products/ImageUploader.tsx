a6da42fdb758aadfe2c00acc15d053b2376c28a5
heejung <acb4287@naver.com>
2023-10-24 21:22:24 +0900

    ƒeat: 기본 스타일링

--- src/components/domains/products/ImageUploader.tsx
+++ src/components/domains/products/ImageUploader.tsx
@@ -1,5 +1,7 @@
 import { FC, useState, useRef, MouseEventHandler, ChangeEventHandler } from 'react';
-import { Flex, Button, Heading, Highlight } from '@chakra-ui/react';
+import { Flex, Button, Image, Badge, Grid, Box, Text } from '@chakra-ui/react';
+
+const MAX = 5;
 
 const ImageUploader: FC = () => {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -14,21 +16,21 @@ const ImageUploader: FC = () => {
     const files = [...(event.target.files ?? [])];
     if (!files.length) return;
     console.log(files);
-    const newImages = Array.from(files).slice(0, 5);
+    const newImages = Array.from(files).slice(0, MAX);
 
-    if (selectedImages.length + files.length <= 5) {
+    if (selectedImages.length + files.length <= MAX) {
       setSelectedImages([...selectedImages, ...newImages]);
       console.log(selectedImages);
     } else {
-      alert(`최대 이미지 파일 5개까지만 선택할 수 있습니다. 선택한 파일 중 5개만 지정하여 업로드하겠습니다.`);
-      const limitImage = Array.from(files).slice(0, 5 - selectedImages.length);
+      alert(`최대 이미지 파일 ${MAX}개까지만 선택할 수 있습니다. 선택한 파일 중 ${MAX}개만 지정하여 업로드하겠습니다.`);
+      const limitImage = Array.from(files).slice(0, MAX - selectedImages.length);
       setSelectedImages([...selectedImages, ...limitImage]);
     }
   };
 
   const handleImageClick = (focusImage: File) => {
     setClickedImage(focusImage);
-    alert('대표사진으로 지정되었습니다');
+    // alert('대표사진으로 지정되었습니다');
   };
 
   const handleImageDelete = (targetImageName: string) => {
@@ -41,45 +43,72 @@ const ImageUploader: FC = () => {
   };
 
   return (
-    <Flex flexDirection="row" flexWrap="wrap">
-      <Flex flexDirection="row" width="100%" justifyContent="center" flexWrap="wrap">
-        {selectedImages.map(image => (
-          <Flex flexDirection="column" h="100px" margin="5px">
-            <img
-              key={image.name}
-              src={URL.createObjectURL(image)}
-              alt={`Selected ${image.name}`}
-              style={{ maxWidth: '100px', height: '50px', margin: '10px', justifyContent: 'center' }}
-              onClick={() => handleImageClick(image)}
-            />
-            <Button color="white" variant="solid" colorScheme="red" onClick={() => handleImageDelete(image.name)}>
-              삭제
-            </Button>
-          </Flex>
-        ))}
-
-        {clickedImage && (
-          <Flex width="100%" height="20vh" justifyContent="center" alignContent="center" margin="10px">
-            <Heading lineHeight="tall" size="md">
-              <Highlight query="대표사진" styles={{ px: '2', py: '1', rounded: 'full', bg: 'brand.100' }}>
+    <Box w="100%">
+      <Flex w="100%" justifyContent="center" flexWrap="wrap" gap={2}>
+        <Flex
+          flex="4"
+          h="265px"
+          position="relative"
+          justifyContent="center"
+          alignItems="center"
+          bg={clickedImage ? undefined : 'gray.100'}
+          borderRadius="4px"
+        >
+          {clickedImage && (
+            <>
+              <Badge fontSize="md" colorScheme="brand" position="absolute" left="1em" top="1em">
                 대표사진
-              </Highlight>
-            </Heading>
-            <img
-              src={URL.createObjectURL(clickedImage)}
-              style={{ maxWidth: '80%', maxHeight: '80%', justifyContent: 'center' }}
-            />
-          </Flex>
-        )}
+              </Badge>
+              <Image src={URL.createObjectURL(clickedImage)} />
+            </>
+          )}
+        </Flex>
+
+        <Grid flex="3" gridTemplateColumns="repeat(3, 1fr)" gridTemplateRows="1fr 1fr" gap={2}>
+          {Array.from({ length: MAX }).map((_, index) => {
+            const image: File | undefined = selectedImages[index];
+
+            return (
+              <Flex
+                flexDirection="column"
+                justifyContent="center"
+                bg={image ? undefined : 'gray.100'}
+                borderRadius="4px"
+              >
+                {image && (
+                  <>
+                    <Image
+                      key={image.name}
+                      src={URL.createObjectURL(image)}
+                      alt={`Selected ${image.name}`}
+                      onClick={() => handleImageClick(image)}
+                      w="100%"
+                      h="auto"
+                      objectFit="contain"
+                    />
+                    <Button size="sm" mt={1} colorScheme="red" onClick={() => handleImageDelete(image.name)}>
+                      삭제
+                    </Button>
+                  </>
+                )}
+              </Flex>
+            );
+          })}
+        </Grid>
       </Flex>
-      <Flex display="fix" justifyContent="center" alignContent="center" flexDirection="row" gap="10px">
+
+      <Flex flexDirection="column" justifyContent="center" alignContent="center" gap="10px" mt={10}>
         <Button color="white" variant="solid" colorScheme="brand" onClick={handleUploadButtonClick}>
           파일 업로드하기
           <input type="file" ref={inputRef} onChange={handleImageChange} hidden multiple accept="image/*" />
         </Button>
-        <p>첨부할 이미지를 5장 이하로 골라주세요! 또, 첨부된 이미지 중 하나만 선택해 대표사진으로 지정해주세요!</p>
+        <Text>
+          첨부할 이미지를 {MAX}장 이하로 골라주세요!
+          <br />
+          또, 첨부된 이미지 중 하나만 선택해 대표사진으로 지정해주세요!
+        </Text>
       </Flex>
-    </Flex>
+    </Box>
   );
 };
 
