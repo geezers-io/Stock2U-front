1136146dc802e9949662dcf0df74c0412c3d88ce
heejung <acb4287@naver.com>
2023-10-24 22:45:41 +0900

    temp

--- src/components/domains/products/ImageUploader.tsx
+++ src/components/domains/products/ImageUploader.tsx
@@ -1,48 +1,57 @@
 import { FC, useState, useRef, MouseEventHandler, ChangeEventHandler } from 'react';
 import { Flex, Button, Image, Badge, Grid, Box, Text } from '@chakra-ui/react';
+import { SimpleFile } from '@/api/@types/File';
+import { FileService } from '@/api/services/File';
 
 const MAX = 5;
 
 const ImageUploader: FC = () => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [images, setImages] = useState<File[]>([]);
-  const [thumbImage, setThumbImage] = useState<File | null>(null);
+  const [images, setImages] = useState<SimpleFile[]>([]);
+  const [thumbImage, setThumbImage] = useState<SimpleFile | null>(null);
 
   const handleUploadButtonClick: MouseEventHandler<HTMLButtonElement> = () => {
     inputRef.current?.click();
   };
 
-  const handleImageChange: ChangeEventHandler<HTMLInputElement> = event => {
-    const newImages = [...(event.target.files ?? [])];
-    if (!newImages.length) return;
+  const handleImageChange: ChangeEventHandler<HTMLInputElement> = async event => {
+    const files = Array.from(event.target.files ?? []);
+    if (!files.length) return;
 
     const prevImageLength = images.length;
-
-    const nextImages = (() => {
-      if (prevImageLength + newImages.length <= MAX) {
-        return [...images, ...newImages];
+    const willAppendImages = (() => {
+      if (prevImageLength + files.length <= MAX) {
+        return files;
       }
       alert(`최대 이미지 파일 ${MAX}개까지만 선택할 수 있습니다. 선택한 파일 중 ${MAX}개만 지정하여 업로드하겠습니다.`);
-      const limitImage = Array.from(newImages).slice(0, MAX - prevImageLength);
-      return [...images, ...limitImage];
+      const limited = files.slice(0, MAX - prevImageLength);
+      return limited;
     })();
 
-    setImages(nextImages);
-    if (prevImageLength === 0) {
-      setThumbImage(nextImages[0]);
+    if (willAppendImages.length === 0) return;
+
+    try {
+      const response = await FileService.uploadFile({ files: willAppendImages });
+      const nextImages = [...images, ...response];
+      setImages(nextImages);
+      if (prevImageLength === 0) {
+        setThumbImage(nextImages[0]);
+      }
+    } catch (e) {
+      alert(e);
     }
   };
 
-  const handleImageClick = (focusImage: File) => {
+  const handleImageClick = (focusImage: SimpleFile) => {
     setThumbImage(focusImage);
     // alert('대표사진으로 지정되었습니다');
   };
 
-  const handleImageDelete = (targetImageName: string) => {
-    const nextImages = images.filter(image => image.name !== targetImageName);
+  const handleImageDelete = (targetImageId: number) => {
+    const nextImages = images.filter(image => image.id !== targetImageId);
     setImages(nextImages);
 
-    if (thumbImage?.name === targetImageName) {
+    if (thumbImage?.id === targetImageId) {
       if (nextImages.length === 0) {
         setThumbImage(null);
       } else {
@@ -68,18 +77,19 @@ const ImageUploader: FC = () => {
               <Badge fontSize="md" colorScheme="brand" position="absolute" left="1em" top="1em">
                 대표사진
               </Badge>
-              <Image src={URL.createObjectURL(thumbImage)} />
+              <Image src={thumbImage.uploadPath} />
             </>
           )}
         </Flex>
 
         <Grid flex="3" gridTemplateColumns="repeat(3, 1fr)" gridTemplateRows="1fr 1fr" gap={2}>
           {Array.from({ length: MAX }).map((_, index) => {
-            const image: File | undefined = images[index];
-            const clicked = !!image && !!thumbImage && image.name === thumbImage.name;
+            const image: SimpleFile | undefined = images[index];
+            const clicked = !!image && !!thumbImage && image.id === thumbImage.id;
 
             return (
               <Flex
+                key={`product-${index}-${image?.id}`}
                 flexDirection="column"
                 justifyContent="center"
                 bg={image ? undefined : 'gray.100'}
@@ -92,16 +102,15 @@ const ImageUploader: FC = () => {
                   <>
                     <Image
                       role="button"
-                      key={image.name}
-                      src={URL.createObjectURL(image)}
-                      alt={`Selected ${image.name}`}
+                      src={image.uploadPath}
+                      alt="Product Image"
                       onClick={() => handleImageClick(image)}
                       w="100%"
                       h="auto"
                       aspectRatio="1/1"
                       objectFit="contain"
                     />
-                    <Button size="sm" mt={1} colorScheme="red" onClick={() => handleImageDelete(image.name)}>
+                    <Button size="sm" mt={1} colorScheme="red" onClick={() => handleImageDelete(image.id)}>
                       삭제
                     </Button>
                   </>
