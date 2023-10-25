7cbb8b9cd8c563f79c54a7bb62827cb4a459f6e6
Ellsy23 <dlwnsgus3153@naver.com>
2023-10-26 00:18:21 +0900

    feat: 유저 액션 도중엔 interval 실행 안되도록 개선

--- src/components/shared/ImageCarousel.tsx
+++ src/components/shared/ImageCarousel.tsx
@@ -1,5 +1,5 @@
-import { useState, useCallback, FC, useEffect } from 'react';
-import { Box, Button, Flex, Image } from '@chakra-ui/react';
+import { useState, useCallback, FC, useEffect, useRef } from 'react';
+import { Button, Flex, Image } from '@chakra-ui/react';
 
 interface Props {
   images: string[];
@@ -8,6 +8,7 @@ interface Props {
 const ImageCarousel: FC<Props> = ({ images }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [touchStartX, setTouchStartX] = useState(0);
+  const autoSlideIntervalRef = useRef<number>();
 
   const nextImage = useCallback(() => {
     setCurrentImageIndex(prev => (prev + 1) % images.length);
@@ -17,15 +18,33 @@ const ImageCarousel: FC<Props> = ({ images }) => {
     setCurrentImageIndex(prev => (prev - 1 + images.length) % images.length);
   };
 
+  const stopAutoSlide = useCallback(() => {
+    clearInterval(autoSlideIntervalRef.current);
+  }, []);
+
+  const startAutoSlide = useCallback(() => {
+    autoSlideIntervalRef.current = setInterval(() => {
+      nextImage();
+    }, 5000);
+  }, [nextImage]);
+
+  const resetAutoSlide = () => {
+    stopAutoSlide();
+    startAutoSlide();
+  };
+
   const handleClickBullet = (index: number) => {
+    resetAutoSlide();
     setCurrentImageIndex(index);
   };
 
-  const handleTouch = (e: React.TouchEvent) => {
+  const handleTouchStart = (e: React.TouchEvent) => {
+    stopAutoSlide();
     setTouchStartX(e.touches[0].clientX);
   };
 
   const handleTouchEnd = (e: React.TouchEvent) => {
+    startAutoSlide();
     const touchEndX = e.changedTouches[0].clientX;
     const deltaX = touchEndX - touchStartX;
     const threshold = 50;
@@ -38,14 +57,12 @@ const ImageCarousel: FC<Props> = ({ images }) => {
   };
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      nextImage();
-    }, 2000);
+    startAutoSlide();
 
     return () => {
-      clearInterval(intervalId);
+      stopAutoSlide();
     };
-  }, [images, nextImage]);
+  }, [startAutoSlide, stopAutoSlide]);
 
   return (
     <Flex
@@ -55,19 +72,14 @@ const ImageCarousel: FC<Props> = ({ images }) => {
       width="100%"
       overflow="hidden"
       textAlign="center"
-      onTouchStart={handleTouch}
+      onTouchStart={handleTouchStart}
       onTouchEnd={handleTouchEnd}
     >
-      <Box
-        flex="1"
-        transition="transform 0.5s ease-in-out"
-        transform={`translateX(-${currentImageIndex * 100}%)`}
-        display="flex"
-      >
+      <Flex flex="1" transition="transform 0.3s ease-in-out" transform={`translateX(-${currentImageIndex * 100}%)`}>
         {images.map((image, index) => (
           <Image key={index} src={image} alt={`Slider Image ${index}`} width="100%" height="auto" />
         ))}
-      </Box>
+      </Flex>
       <Flex display={{ base: 'none', md: 'flex' }} justifyContent="center" gap={1}>
         {Array.from({ length: images.length }).map((_, index) => {
           const isCurrImage = index === currentImageIndex;
