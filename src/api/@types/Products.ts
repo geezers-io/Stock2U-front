1f46fa4e27801a5b6f9d35cd115d6ad26579a41e
heejung <acb4287@naver.com>
2023-10-27 23:38:28 +0900

    feat: 잔여재고 API 코드 작성

--- src/api/@types/Products.ts
+++ src/api/@types/Products.ts
@@ -1,14 +1,57 @@
 import { ProductType, ReservationStatus } from './@enums';
+import { SellerDetails } from './Auth';
 import { SimpleFile } from './File';
 
 export interface ProductSeller {
   id: number;
   username: string;
   phone: string;
-  salesCount: number;
-  reviewCount: number;
+  salesCount?: number; //여기 물음표가 맞을까
+  reviewCount?: number;
 }
 
+//getMainProductsList
+export interface aiRecommends {
+  id: number;
+  latitude: number;
+  longitude: number;
+  title: string;
+  distance: number;
+  createdAt: Date;
+  price: number;
+  expiredAt: Date;
+  productType: ProductType;
+  thumbnailUrl: string;
+}
+
+export interface GetMainProductsPostListRequest {
+  latitude: number;
+  longitude: number;
+}
+
+export interface GetMainProductsPostListResponse {
+  aiRecommends: aiRecommends;
+  deadlines: aiRecommends;
+  myNeighborhoods: aiRecommends;
+}
+
+//createproducts
+export interface CreateProductPostRequest {
+  imageIds: number[];
+  title: string;
+  name: string;
+  price: number;
+  type: ProductType;
+  description: string;
+  productCount: number;
+  expiredAt: Date;
+}
+
+export interface CreateProductsPostResponse {
+  id: string;
+}
+
+//getProductsDetail
 export interface GetProductsDetailRequest {
   id: number;
 }
@@ -18,14 +61,108 @@ export interface GetProductsDetailResponse {
   price: number;
   type: ProductType;
   description: string;
-  onlyOneReserve: boolean;
-  showAccountDetails: boolean;
+  productCount: number;
   expiredAt: Date;
   status?: ReservationStatus; // 예약 상태(예약 한 건만 받기 체크 && 예약 진행 중일 시 표기 됨)
   seller: ProductSeller;
   productImages: SimpleFile[];
 }
 
+//modified
+export interface ModifiedProductsPostRequest {
+  id: number; // 이게 맞을까
+  imageIds: number[];
+  title: string;
+  name: string;
+  price: number;
+  type: ProductType;
+  description: string;
+  productCount: number;
+  expiredAt: Date;
+}
+
+export interface ModifiedProductsPostResponse {
+  imageIds: number[];
+  title: string;
+  name: string;
+  price: number;
+  type: ProductType;
+  description: string;
+  productCount: number;
+  expiredAt: Date;
+  seller: SellerDetails;
+  productsImages: SimpleFile[];
+}
+
+//delete
+export interface DeleteProductsRequest {
+  id: number;
+}
+
+//Filtering
+export interface GetFilteringOfProductsPostListRequest {
+  latitude: number;
+  longitude: number;
+  distance?: number;
+  category: ProductType;
+  minPrice?: number;
+  maxPrice?: number;
+  page: number;
+  /**
+   * @example 0 ~
+   */
+  size: number;
+}
+
+export interface Content {
+  id: number;
+  latitude: number;
+  longitude: number;
+  title: string;
+  distance: number;
+  createdAt: Date;
+  price: number;
+  expiredAt: Date;
+  productType: ProductType;
+  thumbnailUrl: string;
+}
+
+export interface Sort {
+  empty: boolean;
+  sorted: boolean;
+  unsorted: boolean;
+}
+
+export interface Pageable {
+  offset: string;
+  sort: Sort;
+  pageNumber: number;
+  pageSize: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+export interface GetFilteringOfProductsPostListResponse {
+  totalPages: number;
+  totalElements: number;
+  size: number;
+  content: Content;
+  number: number;
+  sort: Sort;
+  pageable: Pageable;
+  first: boolean;
+  last: boolean;
+  numberOfElements: number;
+  empty: boolean;
+}
+
 export interface ProductsClient {
+  getMainPostList(request: GetMainProductsPostListRequest): Promise<GetMainProductsPostListResponse>;
+  createPost(request: CreateProductPostRequest): Promise<CreateProductsPostResponse>;
   getDetail(request: GetProductsDetailRequest): Promise<GetProductsDetailResponse>;
+  modified(request: ModifiedProductsPostRequest): Promise<ModifiedProductsPostResponse>;
+  delete(request: DeleteProductsRequest): Promise<void>;
+  getFilteringOfPostList(
+    request: GetFilteringOfProductsPostListRequest,
+  ): Promise<GetFilteringOfProductsPostListResponse>;
 }
--- src/api/services/Products.ts
+++ src/api/services/Products.ts
@@ -4,7 +4,26 @@ import { axiosInstance } from '@/api/client';
 const ROUTE = 'products';
 
 export const ProductsService: ProductsClient = {
+  getMainPostList: async request => {
+    return await axiosInstance.get(`${ROUTE}`, {
+      params: request,
+    });
+  },
+  createPost: async request => {
+    return await axiosInstance.post(`${ROUTE}`, request);
+  },
   getDetail: async ({ id }) => {
     return await axiosInstance.get(`${ROUTE}/${id}`);
+  },
+  modified: async ({ id, ...request }) => {
+    return await axiosInstance.put(`${ROUTE}/${id}`, request);
+  },
+  delete: async ({ id }) => {
+    return await axiosInstance.delete(`${ROUTE}/${id}`);
+  },
+  getFilteringOfPostList: async request => {
+    return await axiosInstance.get(`${ROUTE}/search`, {
+      params: request,
+    });
   },
 };
