a18d48e6c5e12931364606e647466c133747e32d
LimHeeJung <138123134+heejung0413@users.noreply.github.com>
2023-10-28 02:34:08 +0900

    feat: 잔여재고 API 코드 작성 (#25)
    Co-authored-by: heejung <acb4287@naver.com>
    Co-authored-by: Will Kim <will@chequer.io>

--- src/api/@types/Products.ts
+++ src/api/@types/Products.ts
@@ -1,5 +1,6 @@
 import { ProductType, ReservationStatus } from './@enums';
 import { SimpleFile } from './File';
+import { PageRequest, PageResponse } from '@/api/@types/@shared';
 
 export interface ProductSeller {
   id: number;
@@ -9,23 +10,91 @@ export interface ProductSeller {
   reviewCount: number;
 }
 
-export interface GetProductsDetailRequest {
-  id: number;
-}
-export interface GetProductsDetailResponse {
+export interface ProductDetail {
   id: number;
   title: string;
   price: number;
   type: ProductType;
   description: string;
-  onlyOneReserve: boolean;
-  showAccountDetails: boolean;
-  expiredAt: Date;
+  productCount: number;
+  expiredAt: string;
   status?: ReservationStatus; // 예약 상태(예약 한 건만 받기 체크 && 예약 진행 중일 시 표기 됨)
   seller: ProductSeller;
   productImages: SimpleFile[];
 }
 
+export interface ProductSummary {
+  id: number;
+  productCount: number;
+  distance: number;
+  latitude: number;
+  longitude: number;
+  price: number;
+  title: string;
+  expiredAt: string;
+  createdAt: string;
+  productType: ProductType;
+  thumbnailUrl: string;
+}
+
+export interface GetMainPageProductsRequest {
+  latitude: number;
+  longitude: number;
+}
+
+export interface GetMainPageProductsResponse {
+  aiRecommends: ProductSummary[];
+  deadlines: ProductSummary[];
+  myNeighborhoods: ProductSummary[];
+}
+
+export interface CreateProductRequest {
+  imageIds: number[];
+  title: string;
+  name: string;
+  price: number;
+  type: ProductType;
+  description: string;
+  productCount: number;
+  expiredAt: string;
+}
+
+export interface CreateProductResponse {
+  id: string;
+}
+
+export interface GetProductDetailRequest {
+  id: number;
+}
+
+export interface EditProductRequest extends CreateProductRequest {
+  id: number;
+}
+
+export interface RemoveProductRequest {
+  id: number;
+}
+
+export enum Distance {
+  One = 1,
+  Three = 3,
+  Five = 5,
+  Ten = 10,
+}
+export interface SearchProductsRequest extends PageRequest {
+  latitude: number;
+  longitude: number;
+  distance?: Distance;
+  category?: ProductType;
+  minPrice?: number;
+  maxPrice?: number;
+}
+
 export interface ProductsClient {
-  getDetail(request: GetProductsDetailRequest): Promise<GetProductsDetailResponse>;
+  getMainPageList(request: GetMainPageProductsRequest): Promise<GetMainPageProductsResponse>;
+  create(request: CreateProductRequest): Promise<CreateProductResponse>;
+  getDetail(request: GetProductDetailRequest): Promise<ProductDetail>;
+  edit(request: EditProductRequest): Promise<ProductDetail>;
+  remove(request: RemoveProductRequest): Promise<void>;
+  search(request: SearchProductsRequest): Promise<PageResponse<ProductSummary>>;
 }
--- src/api/services/Products.ts
+++ src/api/services/Products.ts
@@ -4,7 +4,26 @@ import { axiosInstance } from '@/api/client';
 const ROUTE = 'products';
 
 export const ProductsService: ProductsClient = {
+  getMainPageList: async request => {
+    return await axiosInstance.get(`${ROUTE}`, {
+      params: request,
+    });
+  },
+  create: async request => {
+    return await axiosInstance.post(`${ROUTE}`, request);
+  },
   getDetail: async ({ id }) => {
     return await axiosInstance.get(`${ROUTE}/${id}`);
+  },
+  edit: async ({ id, ...request }) => {
+    return await axiosInstance.put(`${ROUTE}/${id}`, request);
+  },
+  remove: async ({ id }) => {
+    return await axiosInstance.delete(`${ROUTE}/${id}`);
+  },
+  search: async request => {
+    return await axiosInstance.get(`${ROUTE}/search`, {
+      params: request,
+    });
   },
 };
