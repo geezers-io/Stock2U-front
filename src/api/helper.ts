099cbf522fba2707dc500870b5bb7490f9606cbb
Will Kim <will@chequer.io>
2023-10-29 05:08:29 +0900

    chore: 개발환경일 경우 에러 메세지에 traceId 표시

--- src/api/helper.ts
+++ src/api/helper.ts
@@ -1,3 +1,5 @@
+const isDev = import.meta.env.DEV;
+
 export function getErrorMessage(err: unknown): string {
   if (typeof err === 'string') {
     return err;
@@ -5,6 +7,7 @@ export function getErrorMessage(err: unknown): string {
   if (typeof err === 'object' && !!err && !Array.isArray(err)) {
     const error: Record<string, any> = err;
     const errorData = error.response?.data?.data ?? error.response?.data ?? error.data ?? error;
+    const traceId = error.response?.headers?.['x-trace-id'];
 
     const message: string | undefined = (() => {
       if (typeof errorData === 'object') return errorData?.message;
@@ -14,12 +17,15 @@ export function getErrorMessage(err: unknown): string {
       return;
     })();
 
+    const withTraceId = (message: string) => (isDev ? `${message} (Trace ID: ${traceId}) ` : message);
+
     if (message) {
       if (error.status === 500) {
-        return `[SERVER ERROR] ${message}`;
+        return `[SERVER ERROR] ${withTraceId(message)}`;
       }
       return message;
     }
+    return withTraceId('알 수 없는 에러 발생');
   }
   return '알 수 없는 에러 발생';
 }
