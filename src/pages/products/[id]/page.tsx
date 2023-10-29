3b2b1da1316efcc0a780c3cfcd61287fe616af8f
heejung <acb4287@naver.com>
2023-10-29 21:26:00 +0900

    feat: 판매자 구독하기 버튼 전환 기능(#2)

--- src/pages/products/[id]/page.tsx
+++ src/pages/products/[id]/page.tsx
@@ -7,7 +7,6 @@ import ImageViewer from '@/components/domains/products/ImageViewer';
 const ProductDetailPage: FC = () => {
   const [productDetail, setProductDetail] = useState<MockProductDetail>();
   const [subscribe, setSubscribe] = useState<boolean>(false);
-  const toast = useCustomToast();
 
   const fetchProductDetail = async () => {
     try {
@@ -24,7 +23,6 @@ const ProductDetailPage: FC = () => {
   }
   const onSubscribe = () => {
     setSubscribe(prevState => !prevState);
-    toast.success('구독되었습니다');
   };
   return (
     <Flex minHeight="inherit" flexDirection="column" justifyContent="space-between">
