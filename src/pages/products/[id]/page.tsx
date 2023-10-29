583ed8a3d232e39a3967c56949d7970ae9d7f523
heejung <acb4287@naver.com>
2023-10-29 23:56:46 +0900

    chore: eslint 막혀서 다시 commit

--- src/pages/products/[id]/page.tsx
+++ src/pages/products/[id]/page.tsx
@@ -14,6 +14,7 @@ import {
   ModalContent,
   ModalFooter,
   ModalHeader,
+  useDisclosure,
 } from '@chakra-ui/react';
 import { ProductType } from '@/api/@types/@enums';
 import { MockProductDetail, mockProductDetail } from '@/api/__mock__/product';
