30af2b182ef7533b8b3750ff4e08e31ca39e1c3b
LimHeeJung <138123134+heejung0413@users.noreply.github.com>
2023-11-07 00:03:39 +0900

    [재고 등록 페이지] formik을 통한 값 유효성 검사 기능 추가 (#31)
    Co-authored-by: heejung <acb4287@naver.com>

--- src/constants/product.ts
+++ src/constants/product.ts
@@ -1,1 +1,6 @@
-export const MIN_PRICE = 1000;
+export const PRODUCT_MIN_PRICE = 1000;
+
+export const PRODUCT_MAX_COUNT = 100;
+
+export const PRODUCT_MAX_TEXT = 1000;
+export const PRODUCT_MIN_TEXT = 3;
