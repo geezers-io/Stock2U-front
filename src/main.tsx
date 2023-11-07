f90973debb3ebaf0ad14b8556e7c81470717158d
galaxy4276 <galaxyhi4276@gmail.com>
2023-11-07 18:14:02 +0900

    feat: MSW 활성화

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ErrorBoundary from '@/components/ErrorBoundary';
+import { mswWorker } from '@/mocks/handler';
+
+// 개발용
+mswWorker.start();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ErrorBoundary>
