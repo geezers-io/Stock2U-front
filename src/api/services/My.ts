e53fc8a9d130e5f079508c8d12e2efe9f5c417fc
LimHeeJung <138123134+heejung0413@users.noreply.github.com>
2023-11-11 03:03:08 +0900

    feat: [구매자 마이페이지] 구독 API (#37)
    Co-authored-by: heejung <acb4287@naver.com>

---
+++ src/api/services/My.ts
@@ -0,0 +1,25 @@
+import { MyClient } from '@/api/@types/My';
+import { axiosInstance } from '@/api/client';
+
+const ROUTE = 'my';
+
+export const MyService: MyClient = {
+  purchaserGetAccountInfo: async () => {
+    return await axiosInstance.get(`${ROUTE}/purchaser`);
+  },
+  purchaserSubscribe: async params => {
+    return await axiosInstance.post(`${ROUTE}/purchaser/subscribe`, null, {
+      params,
+    });
+  },
+  purchaserGetSubscribedProducts: async request => {
+    return await axiosInstance.get(`${ROUTE}/purchaser/subscribe/list`, {
+      params: request,
+    });
+  },
+  purchaserUnsubscribe: async request => {
+    return await axiosInstance.delete(`${ROUTE}/purchaser/unsubscribe`, {
+      params: request,
+    });
+  },
+};
