0f79ed1785eb05b60d4e4babfce841b952616a31
Will Kim <will@chequer.io>
2023-11-14 03:34:28 +0900

    feat: 오버레이에 썸네일 이미지 추가

--- src/components/domains/products/map/ProductMarkerOverlay.tsx
+++ src/components/domains/products/map/ProductMarkerOverlay.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from 'react';
 import { EggFried, HouseDoor, TicketPerforated } from 'react-bootstrap-icons';
 import { CustomOverlayMap } from 'react-kakao-maps-sdk';
 import { Link } from 'react-router-dom';
-import { Box, Flex, LinkBox, Text } from '@chakra-ui/react';
+import { Box, Flex, Image, LinkBox, Text } from '@chakra-ui/react';
 import { useTheme } from '@emotion/react';
 import { ProductType } from '@/api/@types/@enums';
 import { Coordinate } from '@/api/@types/@shared';
@@ -34,39 +34,41 @@ const ProductMarkerOverlay: FC<Props> = ({ latitude, longitude, product }) => {
         lng: longitude,
       }}
       xAnchor={0.5}
-      yAnchor={1.2}
+      yAnchor={1.05}
     >
       <LinkBox
         as={Link}
         to={`/products/${product.id}`}
-        minW="160px"
-        maxW="300px"
-        display="flex"
-        gap={1}
-        bgColor="white"
-        p="5px 12px 5px 5px"
-        borderRadius="999rem"
+        display="block"
         boxShadow="0 0.2rem 0.4rem 0 rgba(0,0,0,.15)"
+        borderRadius="20px"
         border="2px solid"
         borderColor={colorDict[product.productType]}
+        bgColor="white"
       >
-        <Flex
-          justifyContent="center"
-          alignItems="center"
-          borderRadius="999rem"
-          w="28px"
-          bgColor={colorDict[product.productType]}
-        >
-          {iconDict[product.productType]}
-        </Flex>
-        <Box flex={1} lineHeight="1.2">
-          <Text fontSize="xs" fontWeight={700}>
-            {product.title}
-          </Text>
-          <Text fontWeight={500} fontSize="xs" color="gray.500">
-            {processer.price(product.price)} · 남은 수량: {processer.remain(product.productCount, 99)}개
-          </Text>
+        <Box p="5px 5px 2px">
+          <Image src={product.thumbnailUrl} w="100%" height={105} objectFit="cover" borderTopRadius="12px" />
         </Box>
+
+        <Flex w="100%" gap={1} p="5px 12px 5px 5px">
+          <Flex
+            justifyContent="center"
+            alignItems="center"
+            borderRadius="999rem"
+            w="28px"
+            bgColor={colorDict[product.productType]}
+          >
+            {iconDict[product.productType]}
+          </Flex>
+          <Box flex={1} lineHeight="1.2" maxW="10rem">
+            <Text fontSize="xs" fontWeight={700} textOverflow="ellipsis" whiteSpace="nowrap" overflow="hidden">
+              {product.title}
+            </Text>
+            <Text fontWeight={500} fontSize="xs" color="gray.500">
+              {processer.price(product.price)} · 남은 수량: {processer.remain(product.productCount, 99)}개
+            </Text>
+          </Box>
+        </Flex>
       </LinkBox>
     </CustomOverlayMap>
   );
