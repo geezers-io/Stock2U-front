374669ed46a4620b71204e8edded4e6f85d08f0a
Will Kim <will@chequer.io>
2023-11-14 01:27:58 +0900

    feat: 닫기 버튼 포함한 헤더 추가, 레이아웃 작업

--- src/components/domains/products/MapDrawer.tsx
+++ src/components/domains/products/MapDrawer.tsx
@@ -1,8 +1,22 @@
 import { ReactNode, useState } from 'react';
+import { XLg } from 'react-bootstrap-icons';
 import { Map } from 'react-kakao-maps-sdk';
-import { Drawer, DrawerBody, DrawerContent, DrawerOverlay, Text } from '@chakra-ui/react';
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  Heading,
+  IconButton,
+  Text,
+} from '@chakra-ui/react';
+import { useTheme } from '@emotion/react';
 import { Coordinate } from '@/api/@types/@shared';
 import ClusterMarker from '@/components/domains/products/ClusterMarker';
+import PageHeader from '@/components/layouts/parts/PageHeader';
 import { useBoundedStore } from '@/stores';
 import { pick } from '@/utils/object';
 
@@ -26,30 +40,58 @@ const MapDrawer = <T extends Coordinate>({
   renderMarker,
   customOverlay,
 }: Props<T>) => {
+  const theme = useTheme();
   const geo = useBoundedStore(state => state.geo);
   const [level, setLevel] = useState(3);
 
   return (
     <Drawer isOpen={isOpen} onClose={close} placement="bottom">
       <DrawerOverlay />
-      <DrawerContent h="100vh">
-        <DrawerBody transitionDuration="400ms" p={0} position="relative">
-          {!data.length && <Text color="gray.700">지도에 표시할 결과가 없어요.</Text>}
-          {!!data.length && (
-            <Map
-              center={{ lat: geo.latitude, lng: geo.longitude }}
-              level={level}
-              onZoomChanged={map => setLevel(map.getLevel())}
-              style={{ width: '100%', height: '100vh' }}
-            >
-              {level > clusterLevel && (
-                <ClusterMarker coordinates={data.map(item => pick(item, ['latitude', 'longitude']))} />
-              )}
+      <DrawerContent>
+        <DrawerHeader p="unset" h="unset" minH="unset">
+          <PageHeader>
+            <Flex w="100%" position="relative" justifyContent="center" alignItems="center">
+              <IconButton
+                aria-label="Back"
+                position="absolute"
+                left={-2}
+                variant="ghost"
+                icon={<XLg fontSize={24} />}
+                onClick={close}
+              />
+              <Heading fontSize="xl" fontWeight={500}>
+                지도
+              </Heading>
+            </Flex>
+          </PageHeader>
+        </DrawerHeader>
+        <DrawerBody transitionDuration="400ms" p="unset" bgColor="gray.50">
+          <Box
+            m="0 auto"
+            h={{
+              base: `calc(100vh - ${theme.appStyles.headerHeight.base})`,
+              md: `calc(100vh - ${theme.appStyles.headerHeight.md})`,
+            }}
+            maxW={theme.appStyles.maxWidth}
+            position="relative"
+          >
+            {!data.length && <Text color="gray.700">지도에 표시할 결과가 없어요.</Text>}
+            {!!data.length && (
+              <Map
+                center={{ lat: geo.latitude, lng: geo.longitude }}
+                level={level}
+                onZoomChanged={map => setLevel(map.getLevel())}
+                style={{ width: '100%', height: 'inherit' }}
+              >
+                {level > clusterLevel && (
+                  <ClusterMarker coordinates={data.map(item => pick(item, ['latitude', 'longitude']))} />
+                )}
 
-              {level <= clusterLevel && data.map(item => renderMarker({ data: item }))}
-              {level <= clusterLevel && customOverlay}
-            </Map>
-          )}
+                {level <= clusterLevel && data.map(item => renderMarker({ data: item }))}
+                {level <= clusterLevel && customOverlay}
+              </Map>
+            )}
+          </Box>
         </DrawerBody>
       </DrawerContent>
     </Drawer>
