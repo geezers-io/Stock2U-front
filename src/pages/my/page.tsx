95153bb86df9a19327331610077c7a398b9ad954
heejung <acb4287@naver.com>
2023-11-14 17:43:11 +0900

    fix: accordion UI component 삭제

--- src/pages/my/page.tsx
+++ src/pages/my/page.tsx
@@ -1,20 +1,6 @@
 import { FC, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import {
-  Box,
-  Avatar,
-  VStack,
-  HStack,
-  Text,
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon,
-  Divider,
-  Flex,
-  Button,
-} from '@chakra-ui/react';
+import { Box, Avatar, VStack, HStack, Text, Divider, Flex, Button } from '@chakra-ui/react';
 import { PurchaserGetAccountInfo } from '@/api/@types/@shared';
 import { MyService } from '@/api/services/My';
 import { useCustomToast } from '@/hooks/useCustomToast';
@@ -43,47 +29,6 @@ const MyPage: FC = () => {
 
   return (
     <Box display="flex" flexDirection="row" p={4} pt={10}>
-      <VStack align="start" spacing={4} w="150px">
-        <Text fontWeight="bold" fontSize="xl">
-          마이페이지
-        </Text>
-        <Accordion defaultIndex={[0]} allowMultiple>
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">
-                  <Text fontWeight="bold">나의 쇼핑</Text>
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              <Flex direction="column">
-                <Link to={''}>구매 내역</Link>
-                <Link to={''}>예약 내역</Link>
-                <Link to={''}>찜한 상품</Link>
-              </Flex>
-            </AccordionPanel>
-          </AccordionItem>
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">
-                  <Text fontWeight="bold">내 정보</Text>
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              <Flex direction="column">
-                <Link to={''}>정보 관리</Link>
-                <Link to={''}>결제 관리</Link>
-              </Flex>
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      </VStack>
-
       <Box ml={8} w="100%">
         <HStack spacing={4}>
           <Avatar size="lg" name={purchaserInfo.name} src={purchaserInfo.avatarUrl} />
