76635422f78656f799634d37fb92e179b9992226
heejung <acb4287@naver.com>
2023-11-16 00:35:50 +0900

    feat: 마이페이지-판매자 API type, 호출함수 작성

--- src/api/@types/My.ts
+++ src/api/@types/My.ts
@@ -1,4 +1,5 @@
-import { PageRequest, PageResponse, PurchaserGetAccountInfo } from '@/api/@types/@shared';
+import { SellerDetails } from './Auth';
+import { Coordinate, PageRequest, PageResponse, PurchaserGetAccountInfo } from '@/api/@types/@shared';
 import { ProductSummary } from '@/api/@types/Products';
 
 export interface PurchaserSubscribeRequest {
@@ -9,9 +10,28 @@ export interface PurchaserUnsubscribeRequest {
   id: number;
 }
 
+export interface SellerGetAccountInfo extends SellerDetails {
+  location: string;
+  bankName: string;
+  account: string;
+}
+
+export interface sellerBankInfo {
+  bankName: string;
+  account: string;
+}
+
+export interface SellerFixedLocationInfoRequest extends Coordinate {
+  location: string;
+}
+
 export interface MyClient {
   purchaserGetAccountInfo(): Promise<PurchaserGetAccountInfo>;
   purchaserSubscribe(request: PurchaserSubscribeRequest): Promise<void>;
   purchaserGetSubscribedProducts(request: PageRequest): Promise<PageResponse<ProductSummary>>;
   purchaserUnsubscribe(request: PurchaserUnsubscribeRequest): Promise<void>;
+  sellerGetAccountInfo(): Promise<SellerGetAccountInfo>;
+  sellerFixedBankInfo(request: sellerBankInfo): Promise<void>;
+  sellerGetBankInfo(): Promise<sellerBankInfo>;
+  sellerFixedLocationInfo(request: SellerFixedLocationInfoRequest): Promise<void>;
 }
